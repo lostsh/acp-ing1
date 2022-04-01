cef7a32acd490dd8e7ed9c0690b4e6e5b43c4fc2
lostsh <0pdlna3@gmail.com>
2022-04-01 11:18:18 +0200

    small example for Image class usage

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,20 @@
+import data.Image;
+import data.ImageVector;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello fraises");
+
+        /* Just an example of how it works :
+         * Use Image class to import the file
+         * Then we use Image vector to manipulate Image.
+         * (Once you extracted the vector from the Image,
+         * you do not manipulate the Image object)
+         */
+        Image example = new Image("../BDD/Learn/2/20220317_132350.jpg");
+
+        // gui from IJ lib, do not use that, its juste for example
+        // to show you what it look like
+        example.show();
     }
 }
