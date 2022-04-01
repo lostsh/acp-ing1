569bdc09613ed00ab788cf862fb82e52cb96946f
lostsh <0pdlna3@gmail.com>
2022-04-01 11:35:36 +0200

    adding Image example to show how to modify the picture

--- java/src/Main.java
+++ java/src/Main.java
@@ -16,5 +16,32 @@ public class Main {
         // gui from IJ lib, do not use that, its juste for example
         // to show you what it look like
         example.show();
+
+        /* This is how to manipulate an Image
+         * from grey shades Image to b&w Image
+         */
+        byte[] pixels = example.getPixels();
+
+        int width = example.getWidth();
+        int height = example.getHeight();
+
+        //Iterate over the picture
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+
+                //convert byte pixel value to Integer
+                int pix = pixels[ i*width + j ] & 0xff;
+
+                //if px value greater than 128 its black
+                // 128 is an arbitrary value
+                if (pix <= 128)
+                    pixels[i * width + j] = (byte) 0; // white
+                else
+                    pixels[i * width + j] = (byte) 255; // black
+            }
+        }
+
+        // apply modifications to Image
+        example.setPixels(pixels);
     }
 }
