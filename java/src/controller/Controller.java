938b32f60379d89e7f1af406677388d95bd76abc
Yohann Vernhes <yohann.vernhes@eisti.fr>
2022-05-03 23:46:43 +0200

    left pannel work nicelyé

--- java/src/controller/Controller.java
+++ java/src/controller/Controller.java
@@ -9,13 +9,16 @@ import math.EigenMatrix;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Observable;
 
-public class Controller {
+public class Controller extends Observable {
     /**
      * Contains learned matrix
      */
+    private ArrayList<File> learningFiles = null;
     private HashMap<String, ArrayList<ImageVector>> mappy = null;
 
+    private ArrayList<File> testingFiles = null;
     private HashMap<String, ArrayList<ImageVector>> testing = null;
 
     private EigenMatrix matrix = null;
@@ -26,40 +29,71 @@ public class Controller {
     //public static final double EPSILON = 10000;
 
     public void extractLearn(File directory){
+        learningFiles = new ArrayList<>();
+        for (File f : directory.listFiles()) if (f.isFile() && Acp.isImage(f.getName())) learningFiles.add(f);
+        // extract image vectors
         mappy = Acp.extractPicturesVectors(directory.getPath());
-        matrix = Acp.getEigenMatrix(40, mappy);
+        //learn();
+        this.setChanged();
+        this.notifyObservers(learningFiles);
     }
 
-    public boolean saveAverageFace(String savePath){
+    public void learn(){
+        matrix = Acp.getEigenMatrix(40, mappy);
+        this.setChanged();
+        this.notifyObservers();
+    }
+
+    public void saveAverageFace(String savePath){
         if(mappy != null){
             ImageVector averageFace = Acp.averageFace(mappy);
             getImage(averageFace, savePath).save();
-            return true;
+            this.setChanged();
+            this.notifyObservers();
         }
-        return false;
     }
 
-    public boolean saveAverageFace() {
-        return saveAverageFace("../BDD/cropped&gray/average.jpg");
+    public void saveAverageFace() {
+        saveAverageFace("../BDD/cropped&gray/average.jpg");
     }
 
     public void extractTest(File directory){
         if(mappy != null){
             testing = Acp.extractPicturesVectors(directory.getPath());
-            testing = Acp.projectImages(matrix, testing);
+            testingFiles = new ArrayList<>();
+            for (File f : directory.listFiles()) if (f.isFile() && Acp.isImage(f.getName())) testingFiles.add(f);
 
-            mappy = Acp.projectImages(matrix, mappy);
+            //testing = Acp.projectImages(matrix, testing);
+
+            //mappy = Acp.projectImages(matrix, mappy);
+
+            this.setChanged();
+            this.notifyObservers();
         }
+    }
+
+    public void test(){
+        testing = Acp.projectImages(matrix, testing);
+        mappy = Acp.projectImages(matrix, mappy);
+
+        this.setChanged();
+        this.notifyObservers();
     }
 
     public void compareDistances(){
         if(mappy != null && testing != null){
             successRate = Comparison.testDifferentBDDS(testing, mappy, EPSILON);
             successRate = successRate*100;
+
+            this.setChanged();
+            this.notifyObservers();
         }
     }
 
     public double getSuccessRate(){ return successRate; }
+
+    public ArrayList<File> getLearningFiles(){ return learningFiles; }
+    public ArrayList<File> getTestingFiles(){ return testingFiles; }
 
     /**
      * Convert an ImageVector into an Image.
